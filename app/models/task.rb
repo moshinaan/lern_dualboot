e7adec93662f9561fd8f3b6db8d2e543d0c842ce
moshinaan <moshinahacker@yandex.ru>
2019-11-28 17:48:34 +0400

    add state_machines for task

--- app/models/task.rb
+++ app/models/task.rb
@@ -8,4 +8,25 @@ class Task < ApplicationRecord
   validates :description, presence: true
   validates :author, presence: true
   validates :description, length: { maximum: 500 }
+
+  state_machine :state, initial: :new_task do
+  	event :archived do
+      transition [:new_task, :released] => :archived
+    end
+    event :development do
+      transition [:new_task, :in_qa, :in_code_review] => :in_development
+    end
+    event :qa do
+      transition in_development: => :in_qa
+    end
+    event :code_review do
+      transition in_qa: => :in_code_review
+    end
+    event :task_ready_for_release do
+      transition in_code_review: => :ready_for_release
+    end
+    event :task_released do
+      transition ready_for_release: => :released
+    end
+  end
 end
