ae16a4ac6a10c4c2ffb9f7da35e131fe723b25de
moshinaan <moshinahacker@yandex.ru>
2019-12-02 19:01:37 +0400

    update event names

--- app/models/task.rb
+++ app/models/task.rb
@@ -10,22 +10,22 @@ class Task < ApplicationRecord
   validates :description, length: { maximum: 500 }
 
   state_machine :state, initial: :new_task do
-  	event :archived do
+  	event :archive do
       transition [:new_task, :released] => :archived
     end
-    event :development do
+    event :start_development do
       transition [:new_task, :in_qa, :in_code_review] => :in_development
     end
-    event :qa do
+    event :start_qa do
       transition in_development: => :in_qa
     end
-    event :code_review do
+    event :start_code_review do
       transition in_qa: => :in_code_review
     end
-    event :task_ready_for_release do
+    event :ready_for_release do
       transition in_code_review: => :ready_for_release
     end
-    event :task_released do
+    event :released do
       transition ready_for_release: => :released
     end
   end
