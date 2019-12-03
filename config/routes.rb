2e241f9230d4d585a8686b2fb0936382e6451897
moshinaan <moshinahacker@yandex.ru>
2019-12-03 14:37:25 +0400

    pathname error

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,8 @@ Rails.application.routes.draw do
   root :to => "web/board#show"
 
   scope module: :web do
+  	resource :session, only: [:new, :create, :destroy]
     resource :board, only: :show
-    resource :session, only: :new
   end
 end
 
