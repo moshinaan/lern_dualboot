96de87209924f4ea6ec575798a45845b853b832a
moshinaan <moshinahacker@yandex.ru>
2019-12-04 14:34:46 +0400

    test

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -8,6 +8,7 @@ require 'factory_bot_rails'
 class ActiveSupport::TestCase
   # Run tests in parallel with specified workers
   parallelize(workers: :number_of_processors)
+  
 
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
