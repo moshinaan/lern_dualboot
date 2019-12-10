c16b43aa012f2039025db8cdaf3134c773e376f0
moshinaan <moshinahacker@yandex.ru>
2019-12-10 16:05:22 +0400

    .

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   root to: 'web/boards#show'
 
   scope module: :web do
-    resource :sessions, only: %i[new create destroy]
+    resource :session, only: %i[new create destroy]
     resource :boards, only: :show
     resources :developers, only: %i[new create]
   end
