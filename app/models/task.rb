3065e7275b541fb6f94041b981467191b67089e9
moshinaan <moshinahacker@yandex.ru>
2019-12-10 18:35:38 +0400

    fix task  model

--- app/models/task.rb
+++ app/models/task.rb
@@ -9,24 +9,26 @@ class Task < ApplicationRecord
   validates :author, presence: true
   validates :description, length: { maximum: 500 }
 
-  state_machine :state, initial: :new_task do
-  	event :archive do
-      transition [:new_task, :released] => :archived
+  state_machine initial: :new_task do
+    event :start_developm do
+      transition %i[in_qa new_task in_code_review] => :in_development
     end
-    event :start_development do
-      transition [:new_task, :in_qa, :in_code_review] => :in_development
-    end
+
     event :start_qa do
-      transition in_development: => :in_qa
+      transition in_development: :in_qa
     end
     event :start_code_review do
-      transition in_qa: => :in_code_review
+      transition in_qa: :in_code_review
     end
     event :start_release do
-      transition in_code_review: => :ready_for_release
+      transition in_code_review: :ready_for_release
     end
     event :release do
-      transition ready_for_release: => :released
+      transition ready_for_release: :released
+    end
+
+    event :archive do
+      transition %i[new_task released] => :archived
     end
   end
 end
