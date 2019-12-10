15a63207d6444bd4fa242bc66a31e7ddf31f9194
moshinaan <moshinahacker@yandex.ru>
2019-12-10 16:47:49 +0400

    .

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  root to: 'web/board#show'
-
+  root to: 'web/boards#show'
+  post '/board', to: 'web/boards#show'
   scope module: :web do
     resource :session, only: %i[new create destroy]
     resource :board, only: :show
