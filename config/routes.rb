8be202e709c619b721e5f9c71dab2b6f271af98d
moshinaan <moshinahacker@yandex.ru>
2019-12-11 17:57:41 +0400

    fix root

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@
 
 Rails.application.routes.draw do
   root to: 'web/boards#show'
-
+  post '/board', to: 'web/boards#show'
   scope module: :web do
     resource :session, only: %i[new create destroy]
     resource :board, only: :show
