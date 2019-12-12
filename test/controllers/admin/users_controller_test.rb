70ef268060ca64fea5a3367cb3106f60dc424e03
moshinaan <moshinahacker@yandex.ru>
2019-12-12 15:21:45 +0400

    Список пользователей

---
+++ test/controllers/admin/users_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+class Admin::UsersControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get admin_users_url
+    assert_response :success
+  end
+end
