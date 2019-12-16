a792e366ec493a0923112eae81a6584612ebfc4e
moshinaan <moshinahacker@yandex.ru>
2019-12-16 17:04:30 +0400

    fix Api::V1::TasksControlle

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -20,7 +20,6 @@ class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
   include FactoryBot::Syntax::Methods
-
   # Add more helper methods to be used by all tests here...
 end
 
