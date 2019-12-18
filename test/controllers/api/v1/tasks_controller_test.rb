8e442fa0a1c96dae3a0b1d02c7940fbd63d5eb54
moshinaan <moshinahacker@yandex.ru>
2019-12-18 19:19:09 +0400

    fix tasks_controller_test

--- test/controllers/api/v1/tasks_controller_test.rb
+++ test/controllers/api/v1/tasks_controller_test.rb
@@ -27,7 +27,6 @@ class Api::V1::TasksControllerTest < ActionController::TestCase
     data = JSON.parse(response.body)
     created_task = Task.find(data['id'])
 
-    assert created_task.present?
     assert_equal task_attributes.stringify_keys,
                  created_task.slice(*task_attributes.keys)
   end
