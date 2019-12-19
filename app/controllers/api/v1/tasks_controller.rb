fe9000b089183f632542cc634e08afee833a4380
moshinaan <moshinahacker@yandex.ru>
2019-12-19 17:49:40 +0400

    fix tasks_controller.rb

--- app/controllers/api/v1/tasks_controller.rb
+++ app/controllers/api/v1/tasks_controller.rb
@@ -21,7 +21,7 @@ class Api::V1::TasksController < Api::V1::ApplicationController
   end
 
   def create
-    task = current_user.my_tasks.new(task_params)
+    task = current_user.my_tasks.create(task_params)
     respond_with(task, location: nil)
   end
 
