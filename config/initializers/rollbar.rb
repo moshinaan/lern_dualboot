70e6eaebc290fafa66d32b6931b87ae341640fe5
moshinaan <moshinahacker@yandex.ru>
2019-12-23 12:52:56 +0400

    rubocop -a

--- config/initializers/rollbar.rb
+++ config/initializers/rollbar.rb
@@ -7,7 +7,7 @@ Rollbar.configure do |config|
   # Here we'll disable in 'test':
   config.enabled = false if Rails.env.test?
 
-  # By default, Rollbar will try to call the `current_user` controller method 
+  # By default, Rollbar will try to call the `current_user` controller method
   # to fetch the logged-in user object, and then call that object's `id`
   # method to fetch this property. To customize:
   # config.person_method = "my_current_user"
